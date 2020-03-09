2abea311c4ec5626247a9eca3ae0a40bed5ef227
Little-ZYS <1943973549@qq.com>
2020-03-09 23:45:14 +0800

    1

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '-011-zn=pu65)iu$t)i%av=ssy@9jt6im$jc+cch+5pmo4w92n'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost ', '.thezys.com']
 
 
 # Application definition
@@ -120,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
